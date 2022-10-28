56f69711a638ce1733b87bd201930d5ed92275fe
tayeb <tayeb.gasmi@esprit.tn>
2022-10-28 09:50:12 +0100

    module

--- resources/views/modules/create.blade.php
+++ resources/views/modules/create.blade.php
@@ -11,7 +11,7 @@
             @csrf
             <div class="mb-6">
                 <label for="title" class="inline-block text-lg mb-2">Name</label>
-                <input type="text" value="{{ old('name') }}" class="border border-gray-200 rounded p-2 w-full" name="name" placeholder="Example: nodeJS" />
+                <input type="text"  class="border border-gray-200 rounded p-2 w-full" name="name" placeholder="Example: nodeJS" />
                 @error('name')
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -20,22 +20,22 @@
 
             <div class="mb-6">
                 <label for="description" class="inline-block text-lg mb-2">Description</label>
-                <textarea rows="10" type="text" value="{{ old('description') }}" class="border border-gray-200 rounded p-2 w-full" name="description" placeholder="Example: NodeJs"></textarea>
+                <textarea rows="10" type="text"  class="border border-gray-200 rounded p-2 w-full" name="description" placeholder="Example: NodeJs"></textarea>
                 @error('description')
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
             </div>
-            
+
             <div class="mb-6">
                 <label for="image" class="inline-block text-lg mb-2">Image</label>
-                <input type="file" value="{{ old('image') }}" class="border border-gray-200 rounded p-2 w-full" name="image" placeholder="Example: nodeJS" />
+                <input type="file"  class="border border-gray-200 rounded p-2 w-full" name="image" placeholder="Example: nodeJS" />
                 @error('image')
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
             </div>
 
 
-           
+
 
 
             <div class="mb-6">
